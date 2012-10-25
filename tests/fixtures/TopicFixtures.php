44bb7d1106ee9e17c5ba871e4e9bbccb2f6698b4
Golfersds <Golfersds@gmail.com>
2012-10-25 13:21:36 +0300

    Добавил коментарии

--- tests/fixtures/TopicFixtures.php
+++ tests/fixtures/TopicFixtures.php
@@ -4,12 +4,12 @@ require_once(realpath((dirname(__FILE__)) . "/../AbstractFixtures.php"));
 
 class TopicFixtures extends AbstractFixtures
 {
-
+//  порядок создания фикстур
     public static function getOrder()
     {
         return 2;
     }
-
+// Определение параметров для создания фикстур топиков
     public function load()
     {
         $oUserGolfer = $this->getReference('user-golfer');
@@ -35,6 +35,16 @@ class TopicFixtures extends AbstractFixtures
         $this->addReference('topic-sony', $oTopicSony);
     }
 
+    /**
+     *
+     * @param type $iBlogId
+     * @param type $iUserId
+     * @param type $sTitle
+     * @param type $sText
+     * @param type $sTags
+     *
+     * Создание топиков
+     */
     private function _createTopic($iBlogId, $iUserId, $sTitle, $sText, $sTags)
     {
         $oTopic = Engine::GetEntity('Topic');
